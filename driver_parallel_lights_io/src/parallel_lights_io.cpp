306f05691ea262c8b53dbbef05aedc7d1427dc79
vinsfortunato <vinsfortunato97@gmail.com>
2025-06-11 17:18:51 +0200

    feat(driver): implement parallel_lights_io port out.

--- driver_parallel_lights_io/src/parallel_lights_io.cpp
+++ driver_parallel_lights_io/src/parallel_lights_io.cpp
@@ -1,6 +1,10 @@
 #include <parallel_lights_io.h>
 #include <unilight.h>
 
+#define PORT_LPT1 0x378
+#define PORT_LPT2 0x278
+#define PORT_LPT3 0x3BC
+
 #define IS_BIT_SET(data, bit) ((data & (1 << bit)) != 0)
 
 BOOLEAN WINAPI DllMain(IN HINSTANCE hDllHandle, IN DWORD nReason, IN LPVOID Reserved)
@@ -22,41 +26,58 @@ BOOLEAN WINAPI DllMain(IN HINSTANCE hDllHandle, IN DWORD nReason, IN LPVOID Rese
   return TRUE;
 }
 
-/* TODO: Move to PortOut
-bool PacSetLEDStates(int deviceId, short int data) {
-  bool success = true;
-
-  uint8_t pad_lights_state = 0;
-  if(IS_BIT_SET(data, 0)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_UP;
-  if(IS_BIT_SET(data, 1)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_DOWN;
-  if(IS_BIT_SET(data, 2)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_LEFT;
-  if(IS_BIT_SET(data, 3)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_RIGHT;
-  if(IS_BIT_SET(data, 4)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_UP;
-  if(IS_BIT_SET(data, 5)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_DOWN;
-  if(IS_BIT_SET(data, 6)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_LEFT;
-  if(IS_BIT_SET(data, 7)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_RIGHT;
-  if(bd_unilight_set_pad_lights(pad_lights_state) != 0) {
-    success = false;
-  }
-  
+void SetCabLights(char Data) {
   uint8_t cab_lights_state = 0;
-  if(IS_BIT_SET(data, 8)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_UP_LEFT;
-  if(IS_BIT_SET(data, 9)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_UP_RIGHT;
-  if(IS_BIT_SET(data, 10)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_LR_LEFT;
-  if(IS_BIT_SET(data, 11)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_LR_RIGHT;
-  if(IS_BIT_SET(data, 12)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BUTTONS_P1;
-  if(IS_BIT_SET(data, 13)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BUTTONS_P2;
-  if(IS_BIT_SET(data, 14)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BASS;
+  if(IS_BIT_SET(Data, 0)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_UP_LEFT;
+  if(IS_BIT_SET(Data, 1)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_UP_RIGHT;
+  if(IS_BIT_SET(Data, 2)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_LR_LEFT;
+  if(IS_BIT_SET(Data, 3)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_LR_RIGHT;
+  if(IS_BIT_SET(Data, 4)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BUTTONS_P1;
+  if(IS_BIT_SET(Data, 5)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BUTTONS_P2;
+  if(IS_BIT_SET(Data, 6)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BASS;
+  if(IS_BIT_SET(Data, 7)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BASS;
   if(bd_unilight_set_cab_lights(cab_lights_state) != 0) {
-    success = false;
+    // No error handling in this implementation
   }
-
-  return success;
 }
-*/
+
+void SetPad1Lights(char Data) {
+  uint8_t pad_lights_state = 0;
+  if(IS_BIT_SET(data, 0)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_LEFT;
+  if(IS_BIT_SET(data, 1)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_RIGHT;
+  if(IS_BIT_SET(data, 2)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_UP;
+  if(IS_BIT_SET(data, 3)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_DOWN;
+  if(bd_unilight_set_pad_lights(pad_lights_state) != 0) {
+    // No error handling in this implementation
+  }
+}
+
+void SetPad2Lights(char Data) {
+  uint8_t pad_lights_state = 0;
+  if(IS_BIT_SET(data, 0)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_LEFT;
+  if(IS_BIT_SET(data, 1)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_RIGHT;
+  if(IS_BIT_SET(data, 2)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_UP;
+  if(IS_BIT_SET(data, 3)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_DOWN;
+  if(bd_unilight_set_pad_lights(pad_lights_state) != 0) {
+    // No error handling in this implementation
+  }
+}
 
 void PortOut(short int Port, char Data) {
-
+  switch(Port) {
+    case PORT_LPT1:
+      SetCabLights(Data);
+      break;
+    case PORT_LPT2:
+      SetPad1Lights(Data);
+      break;
+    case PORT_LPT3:
+      SetPad2Lights(Data);
+      break;
+    default:
+      // Invalid port
+      break;
+  }
 }
 
 short int IsDriverInstalled() {
