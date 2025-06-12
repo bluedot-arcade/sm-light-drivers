3fddb75efa5c09ca25537bb2bd27f008ae5e1e7f
vinsfortunato <vinsfortunato97@gmail.com>
2025-06-12 11:09:33 +0200

    fix(driver): fix pad lights writing in parallel_lights_io

--- driver_parallel_lights_io/src/parallel_lights_io.cpp
+++ driver_parallel_lights_io/src/parallel_lights_io.cpp
@@ -7,7 +7,22 @@
 #define PORT_LPT2 0x278
 #define PORT_LPT3 0x3BC
 
+#define BD_UNILIGHT_LIGHT_PAD_P1_ALL ( \
+  BD_UNILIGHT_LIGHT_PAD_P1_LEFT  | \
+  BD_UNILIGHT_LIGHT_PAD_P1_RIGHT | \
+  BD_UNILIGHT_LIGHT_PAD_P1_UP    | \
+  BD_UNILIGHT_LIGHT_PAD_P1_DOWN)
+
+#define BD_UNILIGHT_LIGHT_PAD_P2_ALL ( \
+  BD_UNILIGHT_LIGHT_PAD_P2_LEFT  | \
+  BD_UNILIGHT_LIGHT_PAD_P2_RIGHT | \
+  BD_UNILIGHT_LIGHT_PAD_P2_UP    | \
+  BD_UNILIGHT_LIGHT_PAD_P2_DOWN)
+
 #define IS_BIT_SET(data, bit) ((data & (1 << bit)) != 0)
+
+static uint8_t g_cab_lights_state = 0;
+static uint8_t g_pad_lights_state = 0;
 
 BOOLEAN WINAPI DllMain(IN HINSTANCE hDllHandle, IN DWORD nReason, IN LPVOID Reserved)
 {
@@ -30,41 +45,44 @@ BOOLEAN WINAPI DllMain(IN HINSTANCE hDllHandle, IN DWORD nReason, IN LPVOID Rese
   return TRUE;
 }
 
-void SetCabLights(char Data) {
-  uint8_t cab_lights_state = 0;
-  if(IS_BIT_SET(Data, 0)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_UP_LEFT;
-  if(IS_BIT_SET(Data, 1)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_UP_RIGHT;
-  if(IS_BIT_SET(Data, 2)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_LR_LEFT;
-  if(IS_BIT_SET(Data, 3)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_LR_RIGHT;
-  if(IS_BIT_SET(Data, 4)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BUTTONS_P1;
-  if(IS_BIT_SET(Data, 5)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BUTTONS_P2;
-  if(IS_BIT_SET(Data, 6)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BASS;
-  if(IS_BIT_SET(Data, 7)) cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BASS;
-  if(bd_unilight_set_cab_lights(cab_lights_state) != 0) {
+void WriteCabLights() {
+  if(bd_unilight_set_cab_lights(g_cab_lights_state) != 0) {
     // No error handling in this implementation
   }
+}
+
+void WritePadLights() {
+  if(bd_unilight_set_pad_lights(g_pad_lights_state) != 0) {
+    // No error handling in this implementation
+  }
+}
+
+void SetCabLights(char Data) {
+  g_cab_lights_state = 0;
+  if(IS_BIT_SET(Data, 0)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_UP_LEFT;
+  if(IS_BIT_SET(Data, 1)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_UP_RIGHT;
+  if(IS_BIT_SET(Data, 2)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_LR_LEFT;
+  if(IS_BIT_SET(Data, 3)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_MARQUEE_LR_RIGHT;
+  if(IS_BIT_SET(Data, 4)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BUTTONS_P1;
+  if(IS_BIT_SET(Data, 5)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BUTTONS_P2;
+  if(IS_BIT_SET(Data, 6)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BASS;
+  if(IS_BIT_SET(Data, 7)) g_cab_lights_state |= BD_UNILIGHT_LIGHT_CAB_BASS;
 }
 
 void SetPad1Lights(char Data) {
-  uint8_t pad_lights_state = 0;
-  if(IS_BIT_SET(Data, 0)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_LEFT;
-  if(IS_BIT_SET(Data, 1)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_RIGHT;
-  if(IS_BIT_SET(Data, 2)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_UP;
-  if(IS_BIT_SET(Data, 3)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_DOWN;
-  if(bd_unilight_set_pad_lights(pad_lights_state) != 0) {
-    // No error handling in this implementation
-  }
+  g_pad_lights_state &= ~(BD_UNILIGHT_LIGHT_PAD_P1_ALL);
+  if(IS_BIT_SET(Data, 0)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_LEFT;
+  if(IS_BIT_SET(Data, 1)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_RIGHT;
+  if(IS_BIT_SET(Data, 2)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_UP;
+  if(IS_BIT_SET(Data, 3)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P1_DOWN;
 }
 
 void SetPad2Lights(char Data) {
-  uint8_t pad_lights_state = 0;
-  if(IS_BIT_SET(Data, 0)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_LEFT;
-  if(IS_BIT_SET(Data, 1)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_RIGHT;
-  if(IS_BIT_SET(Data, 2)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_UP;
-  if(IS_BIT_SET(Data, 3)) pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_DOWN;
-  if(bd_unilight_set_pad_lights(pad_lights_state) != 0) {
-    // No error handling in this implementation
-  }
+  g_pad_lights_state &= ~(BD_UNILIGHT_LIGHT_PAD_P2_ALL);
+  if(IS_BIT_SET(Data, 0)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_LEFT;
+  if(IS_BIT_SET(Data, 1)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_RIGHT;
+  if(IS_BIT_SET(Data, 2)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_UP;
+  if(IS_BIT_SET(Data, 3)) g_pad_lights_state |= BD_UNILIGHT_LIGHT_PAD_P2_DOWN;
 }
 
 short int WINAPI IsDriverInstalled(){
@@ -78,12 +96,14 @@ void WINAPI PortOut(short int port, char data)
   switch(port) {
     case PORT_LPT1:
       SetCabLights(data);
+      WriteCabLights();
       break;
     case PORT_LPT2:
       SetPad1Lights(data);
       break;
     case PORT_LPT3:
       SetPad2Lights(data);
+      WritePadLights(); // Write only after both pads are set.
       break;
     default:
       break;
